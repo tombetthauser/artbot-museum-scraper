38c344e7b0b7be0f443ff50746d1df6947045795
tombetthauser <tombetthauser@gmail.com>
2021-09-05 10:32:25 -0700

    Update this repo

--- scripts/vibrate.js
+++ scripts/vibrate.js
@@ -4,9 +4,19 @@ console.log("VIBRATE.JS START:", randId);
 
 let count = 0;
 
+const topMargin = Math.floor(Math.random() * 500);
+const leftMargin = Math.floor(Math.random() * 1000);
+
+const yVibration = Math.floor(Math.random() * 50);
+const xVibration = Math.floor(Math.random() * 50);
+
+liveDiv.style.transition = `${Math.floor(Math.random() * 100)}ms`;
+
 const interval = setInterval(() => {
   console.log("INTERVAL RUN:", count);
-  console.log("INTERVAL RUN:", count);
-  liveDiv.style.top = `${Math.floor(Math.random() * 20)}px`;
-  liveDiv.style.left = `${Math.floor(Math.random() * 30)}px`;
+  console.log("INTERVAL ID:", randId);
+  count++;
+  liveDiv.style.top = `${Math.floor(Math.random() * yVibration) + topMargin}px`;
+  liveDiv.style.left = `${Math.floor(Math.random() * xVibration) + leftMargin}px`;
+  liveDiv.style.boxShadow = `${Math.floor(Math.random() * xVibration)}px`;
 }, 20);
